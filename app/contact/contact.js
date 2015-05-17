cf9f49be7531bd013e7a073ef4496fd6a7813192
Thomas Brison <th.brison@gmail.com>
2015-05-17 21:15:54 +0200

    Start to templatize contact

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -2,13 +2,21 @@
 
 angular.module('myCVApp.contact', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/contact', {
-    templateUrl: 'contact/contact.html',
-    controller: 'ContactCtrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/contact', {
+                    templateUrl: 'contact/contact.html',
+                    controller: 'ContactCtrl'
+                });
+            }])
 
-.controller('ContactCtrl', [function() {
-
-}]);
+        .controller('ContactCtrl', ['$scope', function ($scope) {
+                $scope.mobilePhone = "06.78.72.26.24";
+                $scope.email = "thomas@brison.me";
+                $scope.address = {
+                    number: 269,
+                    streetType: "rue",
+                    streetName: "André Philip",
+                    postalCode: "69003",
+                    city: "Lyon"
+                };
+            }]);
